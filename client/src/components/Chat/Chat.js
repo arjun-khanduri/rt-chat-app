8e97d85bfb93dae236b5b8e7606cd056eef0b1ac
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-06-23 00:49:40 +0530

    Front-end messaging enabled

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css';
 
 let socket;
 
 const Chat = () => {
     const [room, setRoom] = useState('');
     const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState('');
     const ENDPOINT = 'localhost:8000';
     useEffect(() => {
         const { name, room } = queryString.parse(window.location.search);
@@ -22,10 +25,33 @@ const Chat = () => {
         }
 
     }, [ENDPOINT, window.location.search]);
-    return (
-        <div>
-            <h1>Chat</h1>
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+        return() => {
+            socket.off();
+        }
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message)
+            socket.emit('sendMessage', message, () => setMessage(''));
+    }
+
+    console.log(message, messages);
+
+    return (
+        <div className='App'>
+            <div className='container'>
+                <input value={message} 
+                onChange={(event) => setMessage(event.target.value)} 
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                />
+            </div>
         </div >
     )
 }
