3d6d2181d93ffbcc4d10b17014dfcfaf16c31c76
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-06-23 00:28:42 +0530

    Integrated logic with socket emitters

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 8000;
 
 const router = require('./router');
@@ -17,16 +19,28 @@ const server = http.createServer(app);
 const io = socketio(server, corsOptions);
 
 io.on('connect', (socket) => {
-    console.log('We have a new connection');
+    // console.log('We have a new connection');
 
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { err, user } = addUser({ id: socket.id, name, room });
+        if (err)
+            return callback(err);
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the chat` });
+        socket.join(user.room);
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        callback();
     });
 
     socket.on('disconnect', () => {
         console.log('User has left');
     })
-})
+});
 
 app.use(router);
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
