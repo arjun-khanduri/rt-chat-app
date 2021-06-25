614e9d8a5a1704c2f021987a057707716e8aeb08
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-06-26 01:25:48 +0530

    Changed Room textbox to dropdown

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -11,7 +11,15 @@ const Join = () => {
             <div className='joinInnerContainer'>
                 <h1 className='heading'>Join</h1>
                 <div><input placeholder='Name' className='joinInput' type='text' onChange={(event) => setName(event.target.value)}/></div>
-                <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(event) => setRoom(event.target.value)}/></div>
+                {/* <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(event) => setRoom(event.target.value)}/></div> */}
+                <div><select className='joinInput mt-20' onChange={(event) => setRoom(event.target.value)}>
+                        <option value='null'>Select a room</option>
+                        <option>Room A</option>
+                        <option>Room B</option>
+                        <option>Room C</option>
+                        <option>Room D</option>
+                        <option>Room E</option>
+                    </select></div>
                 <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                     <button className='button mt-20' type='submit'>Join</button>
                 </Link>   
