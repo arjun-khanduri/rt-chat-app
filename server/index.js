34aa98b56131ffc5942609dda0e93c8471c99063
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-06-26 01:41:11 +0530

    Changed server default message

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ io.on('connect', (socket) => {
         const { error, user } = addUser({ id: socket.id, name, room });
         if (error)
             return callback(error);
-        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}` });
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to ${user.room}` });
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the chat` });
         socket.join(user.room);
         io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
