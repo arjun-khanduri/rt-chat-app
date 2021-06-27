a1af364f251123cb081bce5846a37a405e88b692
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-06-27 12:58:50 +0530

    Created and routed landing page

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Server is online');
+    res.render('index.ejs');
 });
 
 module.exports = router;
